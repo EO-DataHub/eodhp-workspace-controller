149d0f52559cdc74819c4caf623c5edc568e9ffc
jonnylangstone <jonny.langstone@telespazio.com>
2024-10-18 13:31:24 +0100

    extended workspace CRD with 3 account and authorization fields

--- api/v1alpha1/workspace_types.go
+++ api/v1alpha1/workspace_types.go
@@ -58,6 +58,16 @@ type WorkspaceSpec struct {
 	ServiceAccount ServiceAccountSpec `json:"serviceAccount,omitempty"`
 	// Storage
 	Storage StorageSpec `json:"storage,omitempty"`
+	// Account associated with the workspace
+	Account string `json:"account,omitempty"`
+	// Owner of the account
+	AccountOwner string `json:"accountOwner,omitempty"`
+	// Authorization details for the workspace
+	Authorization AuthorizationSpec `json:"authorization,omitempty"`
+}
+
+type AuthorizationSpec struct {
+	MemberGroup string `json:"memberGroup,omitempty"`
 }
 
 type StorageSpec struct {
